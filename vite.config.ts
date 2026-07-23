43029d34a5ef4b6d4a48adcfa047e234898cee5f
Alfredo Marvulle <alfredo.marvulle@gmail.com>
2026-07-22 22:09:28 -0300

    ops: fix deploy script for ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/teste-front-end-econverse/",
   plugins: [react()],
   server: {
     proxy: {
