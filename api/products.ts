cde4a4615ba2dd056677f279a24682c0b7623674
Alfredo Marvulle <alfredo.marvulle@gmail.com>
2026-07-22 23:15:26 -0300

    feat: add proxy to vercel

---
+++ api/products.ts
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const response = await fetch(
+    "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
+  );
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
